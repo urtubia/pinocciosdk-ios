0f06a4ca88d78bfe5c2ec962b7f150e0822920d0
Hector Urtubia <urtubia@bigrobotstudios.com>
2014-04-20 07:18:17 -0700

    Deleting empty files for BRSRestClient

--- PinoccioSDK/BRSRestClient.m
+++
@@ -1,9 +0,0 @@
-//
-//  BRSRestClient.m
-//  PinoccioSDK
-//
-//  Created by hector urtubia on 4/13/14.
-//  Copyright (c) 2014 Big Robot Studios, LLC. All rights reserved.
-//
-
-#import "BRSRestClient.h"
